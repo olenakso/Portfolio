4a6dc93f38cae9abada9fd42e0a6f2155f92268d
Olena Ksondzyk <babakina.lena1@gmail.com>
2018-07-09 11:46:32 +0300

    new carusel

--- second page/script.js
+++ second page/script.js
@@ -1,8 +1,31 @@
-var button = document.getElementById("up");
-window.onscroll = function up(){
-   if(window.pageYOffset<200) {
-       button.style.display = "none"
-   }else{
-    button.style.display = "block"
-   }
+
+
+
+var carusel = document.getElementById("myCarousel");
+var close = document.getElementById("close1")
+var img = document.getElementsByClassName("img-one");
+var buttons = document.getElementById("buttonss");
+
+for(var i=0; i<img.length; i++){
+    img[i].addEventListener("click",function func(){
+        close.style.display = "block";
+        close.style.position = "absolute";
+        close.style.top = "30%";
+        close.style.left = "2%"
+        close.style.height = "1500px;"
+    })
 }
+
+buttons.addEventListener("click",function(){
+    close.style.display = "none";
+    
+    })
+
+    var button = document.getElementById("up");
+    window.onscroll = function up(){
+       if(window.pageYOffset>200) {
+           button.style.display = "block"
+       }else{
+        button.style.display = "none"
+       }
+    }
